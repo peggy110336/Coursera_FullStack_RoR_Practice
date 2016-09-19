47fdd078c0a9226f0021adcacada9a56fc51c53e
K-Peggy <peggy110336@gmail.com>
2016-09-20 02:07:42 +0800

    compelete note.

--- model3/mpdel3-note.rb
+++ model3/mpdel3-note.rb
@@ -1,2 +1,15 @@
 puts "Rails is a dynamic web application."
 
+puts "MVC:Model View Control."
+
+puts "Server looks in Public before look anywhere."
+
+puts "產生一個controller and action. 指令：「rails g controller [controller name] [Action name] 」"
+
+puts "The View file is .erb extension. ERB is a templating library that lets you embed Ruby into HTML file."
+
+puts "Two tag patterns to learn："
+puts "1.<%...ruby code...%> -> evaluate Ruby code. 2.<%=...ruby code...%> -> output evaluate Ruby code."
+
+puts "Add new view by self,but it error. Because the routes set the path [greeter/hello]. So,we have to add the path to goodbye's view."
+puts "rake routes 指令查看已設定的view's routes."
